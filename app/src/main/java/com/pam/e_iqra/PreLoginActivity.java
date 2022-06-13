da2d52435838991f241693c33a0dd7abdcc2e855
ahmadmm01 <skyahmad74@gmail.com>
2022-06-13 17:40:10 +0700

    Update Login Layout

--- app/src/main/java/com/pam/e_iqra/PreLoginActivity.java
+++ app/src/main/java/com/pam/e_iqra/PreLoginActivity.java
@@ -1,13 +1,41 @@
 package com.pam.e_iqra;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PreLoginActivity extends AppCompatActivity {
+
+    Button Teacher_button, Student_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_login);
+
+        Teacher_button = findViewById(R.id.button_Teacher);
+        Student_button = findViewById(R.id.button_Student);
+
+        Teacher_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent teacher = new Intent(getApplicationContext(), TeacherLoginActivity.class);
+
+                startActivity(teacher);
+            }
+        });
+
+        Student_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent student = new Intent(getApplicationContext(), StudentLoginActivity.class);
+
+                startActivity(student);
+            }
+        });
+
     }
 }
