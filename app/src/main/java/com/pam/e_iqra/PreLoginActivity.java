cfaa7c99a20a0d1b97dc1c699385b9b9d0ff49ea
ahmadmm01 <skyahmad74@gmail.com>
2022-06-13 12:29:12 +0700

    Update Login Layout

---
+++ app/src/main/java/com/pam/e_iqra/PreLoginActivity.java
@@ -0,0 +1,13 @@
+package com.pam.e_iqra;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pre_login);
+    }
+}
---
+++ app/src/main/java/com/pam/e_iqra/TeacherLoginActivity.java
@@ -0,0 +1,14 @@
+package com.pam.e_iqra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeacherLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_login);
+    }
+}
